a6b317896408b725838b4014fb56e0ff8dc525af
Nic Goodman <nicgoodman@Nics-MBP.lan>
2020-02-05 20:01:09 -0500

    updated tailwind with colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '6rem',
+      },
+      colors: {
+        'nb-dark-blue': '#002A4E',
+        'nb-brown': '#7F341A',
+        'nb-light-blue': '#659EC8',
+        'nb-yellow': '#FFCB58',
+        'nb-mint': '#E3ECEC',
+        'nb-pink': '#FDE6E8',
+      }
+    },
+    fontFamily: {
+      'sans': ['proxima-nova, sans-serif'],
+      'serif': ['chaparral-pro, serif'],
+    }
+
   },
   variants: {},
   plugins: [],
